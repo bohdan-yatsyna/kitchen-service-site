7470f826b96f931eb618d9a3cef92f70bf8a2f58
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-05-30 16:47:56 +0300

    Create models for app kitchen according to project architecture

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from kitchen_service import settings
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        to=DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        related_name="dishes",
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, type: {self.dish_type.name})"
