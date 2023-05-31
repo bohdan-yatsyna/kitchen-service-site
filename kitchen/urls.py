48c1ce80e6801d62101a07055273cce5efab69db
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-05-31 14:56:37 +0300

    Add DishTypeListView with corresponding template, path and sidebar link update

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+
 ]
 
 app_name = "kitchen"
