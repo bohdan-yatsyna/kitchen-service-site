6ee5abbeb954c4b336230831d9e8fd204905e9c2
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-06-01 17:31:21 +0300

    Add DishSerchForm to forms.py in app

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -74,3 +74,14 @@ class DishTypeSearchForm(forms.Form):
             attrs={"placeholder": "Search by name..."}
         )
     )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name..."}
+        )
+    )
