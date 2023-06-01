feeb26a6b6bc3a514ab908110c994e4331713a6a
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-06-01 16:43:53 +0300

    Add Cook Creation and Update forms

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from kitchen.models import Dish
+from kitchen.models import Dish, Cook
 
 
 class DishForm(forms.ModelForm):
@@ -23,3 +24,38 @@ class DishForm(forms.ModelForm):
             raise ValidationError("Price has to be greater than 0.")
 
         return price
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "years_of_experience",
+        )
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+class CookUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "years_of_experience"
+        )
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+def validate_years_of_experience(years_of_experience):
+    if years_of_experience not in range(1, 77):
+        raise ValidationError("Please, enter a valid number")
+
+    return years_of_experience
