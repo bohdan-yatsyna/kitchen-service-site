ea4a10e561e666ecb5c57c667699237317a1af40
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-06-13 16:46:17 +0300

    Update settings.py and requirements.txt

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -27,11 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
+# SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
